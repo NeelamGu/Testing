5f04cdd545465a98ea322c9c1e15b004d48ad2c5
PrabhRtpl <prabhjot@rtpltech.com>
2024-12-09 13:30:56 +0530

    text upadtee

--- resources/views/front/index.blade.php
+++ resources/views/front/index.blade.php
@@ -186,8 +186,8 @@
             </video>
       <div class="auto-container">
          <div class="banner-text">
-            <h2>Gjør det <br> uforglemmelig!</h2>
-            <p>Trenger du noe til ditt neste arrangement? Beskriv dine ønsker og få leverandører fra hele landet til å kontakte deg.<br> Helt gratis og uforpliktende!</p>
+            <h2>Trenger du noe  <br> til ditt neste arrangement?</h2>
+            <p>Beskriv dine ønsker og få leverandører fra hele landet til å kontakte deg.<br> Helt gratis og uforpliktende!</p>
             @if(!isset(Auth::guard('admin')->user()->type))<a class="banner-enquery-btn" href="{{ url('/enquire-us')}}">Legg ut oppdrag</a>@endif
          </div>
       </div>
