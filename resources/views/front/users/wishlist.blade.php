88ac986a2e878784a7ffd65faa385686bf7eb9fd
PrabhRtpl <prabhjot@rtpltech.com>
2025-02-01 15:47:37 +0530

    wishlist update

--- resources/views/front/users/wishlist.blade.php
+++ resources/views/front/users/wishlist.blade.php
@@ -30,31 +30,36 @@
          <div class="row">
             @if(count($wishlists) >0)
             @foreach($wishlists as $wishlist)
-               <!-- loop starts here -->
-               <div class="col-lg-3 col-sm-6">
-                  <div class="wlist-area">
-                     <div class="event-item">
-                        <div class="event-thumb">
-                           @php $getProductURL = Product::productURL($wishlist['product']['product_name']) @endphp
-                           <?php $product_image_path = 'front/images/product_images/small/'.$wishlist['product']['product_image']; ?>
-                           @if(!empty($wishlist['product']['product_image']) && file_exists($product_image_path))
-                              <a href="{{ url('product/'.$getProductURL.'/'.$wishlist['product']['id']) }}"><img class="img-responsive img-fullwidth" src="{{ asset('front/images/product_images/large/'.$wishlist['product']['product_image']) }}"></a>
-                           @else
-                              <a href="{{ url('product/'.$getProductURL.'/'.$wishlist['product']['id']) }}"><img class="img-responsive img-fullwidth" src="{{ asset('front/images/product_images/small/no-image.png') }}"></a>
-                           @endif
-                           <a onclick="return confirm('Are you sure?')" href="{{url('user/remove-wishlist/'.$wishlist['id'])}}">
+            <!-- loop starts here -->
+            <div class="col-lg-3 col-sm-6">
+               <div class="wlist-area">
+                  <div class="event-item">
+                     <div class="event-thumb">
+                        @php $getProductURL = Product::productURL($wishlist['product']['product_name']) @endphp
+                        <?php $product_image_path = 'front/images/product_images/small/'.$wishlist['product']['product_image']; ?>
+                        @if(!empty($wishlist['product']['product_image']) && file_exists($product_image_path))
+                        <a href="{{ url('product/'.$getProductURL.'/'.$wishlist['product']['id']) }}"><img
+                              class="img-responsive img-fullwidth"
+                              src="{{ asset('front/images/product_images/large/'.$wishlist['product']['product_image']) }}"></a>
+                        @else
+                        <a href="{{ url('product/'.$getProductURL.'/'.$wishlist['product']['id']) }}"><img
+                              class="img-responsive img-fullwidth"
+                              src="{{ asset('front/images/product_images/small/no-image.png') }}"></a>
+                        @endif
+                        <a onclick="return confirm('Are you sure?')"
+                           href="{{url('user/remove-wishlist/'.$wishlist['id'])}}">
                            <button name="submitbtn" type="button" value="REMOVE" class="wishlist-icon">
-                           <i class="fa fa-trash" aria-hidden="true"></i>
+                              <i class="fa fa-trash" aria-hidden="true"></i>
                            </button>
+                        </a>
+                     </div>
+                     <div class="event-details">
+                        <div class="wishlist-page-title">
+                           <a href="">
+                              <h4 class="text-thm pb-5 font-weight-700">{{$wishlist['product']['product_name']}}</h4>
                            </a>
                         </div>
-                        <div class="event-details">
-                           <div class="wishlist-page-title">
-                              <a href="">
-                                 <h4 class="text-thm pb-5 font-weight-700">{{$wishlist['product']['product_name']}}</h4>
-                              </a>
-                           </div>
-                           <!-- <div class="wl-price">
+                        <!-- <div class="wl-price">
                               <p class="price">
                                  <span  class="wl-price-area"> 
                                  {{$wishlist['product']['product_price']}}
@@ -62,37 +67,45 @@
                                  kr
                               </p>
                            </div> -->
-                           <div class="price-area price-other">
-                                 @if(isset($wishlist['product']['price_range'])&&$wishlist['product']['price_range']!="")
-                                 @if($wishlist['product']['price_range']=="Low")
-                                 <div class="price-d-area">
-                                    <span class="price-dark">$</span>
-                                    <span>$$</span>
-                                 </div>
-                                 @elseif($wishlist['product']['price_range']=="Medium")
-                                 <div class="price-d-area">
-                                    <span class="price-dark">$$</span>
-                                    <span>$</span>
-                                 </div>
-                                 @elseif($wishlist['product']['price_range']=="High")
-                                 <div class="price-d-area">
-                                    <span class="price-dark">$$$</span>
-                                 </div>
-                                 @endif 
-                                 @else
-                                 <div class="price-d-area">
-                                    <span>$$$</span>
-                                 </div>
-                                 @endif
-                              </div>
-                           <!-- <div class="text-left pt-1">
+                        <div class="price-area price-other">
+                           @if(isset($wishlist['product']['price_range'])&&$wishlist['product']['price_range']!="")
+                           @if($wishlist['product']['price_range']=="Low")
+                           <div class="price-d-area">
+                              <span class="price-dark">$</span>
+                              <span>$$</span>
+                           </div>
+                           @elseif($wishlist['product']['price_range']=="Medium")
+                           <div class="price-d-area">
+                              <span class="price-dark">$$</span>
+                              <span>$</span>
+                           </div>
+                           @elseif($wishlist['product']['price_range']=="High")
+                           <div class="price-d-area">
+                              <span class="price-dark">$$$</span>
+                           </div>
+                           @endif
+                           @else
+                           <div class="price-d-area">
+                              <span>$$$</span>
+                           </div>
+                           @endif
+                        </div>
+                        <!-- <div class="text-left pt-1">
                               <a href="{{ url('product/'.$getProductURL.'/'.$wishlist['product']['id']) }}">
                               <button name="submitbtn" type="button" class="wlist-btn" value="ADD TO CART">View Details</button></a>
                            </div> -->
-                        </div>
+                        <address class="text-dark font-14 mb-10 detail-adress">
+                           <i class="fa fa-map-marker text-thm"></i><span class="pl-5">Notodden&nbsp;&nbsp;</span>
+                           <div class="p-detail-category">
+                              <img class="category-img-icon"
+                                 src="http://localhost:8000/front/images/category_images/40991.png">
+                              <span>Desserter</span>
+                           </div>
+                        </address>
                      </div>
                   </div>
                </div>
+            </div>
             @endforeach
             @endif
          </div>
