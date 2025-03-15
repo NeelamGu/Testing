caa8297304c6d2904a665feb12c05e2f8ebeba63
amitrtpl <amit@rtpltech.com>
2025-03-15 11:08:32 +0530

    testing cities api sync

--- routes/api.php
+++ routes/api.php
@@ -17,3 +17,8 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+Route::prefix('v1')->namespace('App\Http\Controllers\API')->group(function () {
+        //Uniware Apis
+    Route::post('/sync-pincodes','APIController@syncPincodes');
+});
